4a54a796f20a744ff3661606acd090691ac7ce09
Gultzz <83468422+Gultzz@users.noreply.github.com>
2021-07-22 12:47:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -34,57 +34,10 @@ function playComposition(songArray){
     }
 }
 function trocarCor(){
-    var corDigitada = document.getElementById("textCor").value;
+    var colorid = document.getElementById("textCor").value;
     var boy = document.getElementById("boy").style;
-        boy.backgroundColor = corDigitada;
-        if(corDigitada === "verde"){
-            boy.backgroundColor = "green";
-        }else if(corDigitada === "vermelho"){
-            boy.backgroundColor = "red";
-        }else if(corDigitada === "azul"){
-            boy.backgroundColor = "blue";
-        }else if(corDigitada === "amarelo"){
-            boy.backgroundColor = "yellow";
-        }else if(corDigitada === "rosa"){
-            boy.backgroundColor = "pink";
-        }else if(corDigitada === "cinza"){
-            boy.backgroundColor = "gray";
-        }else if(corDigitada === "preto"){
-            boy.backgroundColor = "black";
-        }else if(corDigitada === "roxo"){
-            boy.backgroundColor = "purple";
-        }else if(corDigitada === "laranja"){
-            boy.backgroundColor = "orange";
-        }else if(corDigitada === "branco"){
-            boy.backgroundColor = "white";
-        }else if(corDigitada === "green"){
-            boy.backgroundColor = "green";
-        }else if(corDigitada === "orange"){
-            boy.backgroundColor = "orange";
-        }else if(corDigitada === "black"){
-            boy.backgroundColor = "black";
-        }else if(corDigitada === "yellow"){
-            boy.backgroundColor = "yellow";
-        }else if(corDigitada === "red"){
-            boy.backgroundColor = "red";
-        }else if(corDigitada === "blue"){
-            boy.backgroundColor = "blue";
-        }else if(corDigitada === "gray"){
-            boy.backgroundColor = "gray";
-        }else if(corDigitada === "purple"){
-            boy.backgroundColor = "purple";
-        }else if(corDigitada === "pink"){
-            boy.backgroundColor = "pink";
-        }else if(corDigitada === "pink"){
-            boy.backgroundColor = "pink";
-        }else if(corDigitada === "marrom"){
-            boy.backgroundColor = "rgb(65, 32, 1)";
-        }else if(corDigitada === "brown"){
-            boy.backgroundColor = "rgb(65, 32, 1)";
-        }
-        else{
-            alert("Se a cor que voce deseja aplicar não funcionou," +
-            " teste coloca-lá em ingles e sem espaços");
-        }
+    document.getElementById("inner").innerHTML = colorid;
+        boy.backgroundColor = colorid;
+    
     
 }
